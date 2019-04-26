a0a6e99355f99e58c9b13a0a30d7ae728b666b58
SapuSeven <sapuseven@gmail.com>
2019-04-26 12:26:46 +0200

    RoomFinder code cleanup

--- app/src/main/java/com/sapuseven/untis/activities/RoomFinderActivity.kt
+++ app/src/main/java/com/sapuseven/untis/activities/RoomFinderActivity.kt
@@ -43,7 +43,6 @@ class RoomFinderActivity : BaseActivity(), ElementPickerDialog.ElementPickerDial
 	private var roomListMargins: Int = 0
 	private var hourIndex: Int = 0
 	private var maxHourIndex = 0
-	//private var dialog: AlertDialog? = null
 	private var roomList: MutableList<RoomFinderAdapterItem> = ArrayList()
 	private var roomListAdapter = RoomFinderAdapter(this, this)
 	private var profileUser: User? = null
@@ -53,7 +52,7 @@ class RoomFinderActivity : BaseActivity(), ElementPickerDialog.ElementPickerDial
 
 	companion object {
 		const val EXTRA_LONG_PROFILE_ID = "profile_id"
-		const val EXTRA_INT_ROOM_ID = "roomId"
+		const val EXTRA_INT_ROOM_ID = "room_id"
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -173,11 +172,11 @@ class RoomFinderActivity : BaseActivity(), ElementPickerDialog.ElementPickerDial
 	}
 
 	override fun onDialogDismissed(dialog: DialogInterface?) {
-
+		// Nothing to do
 	}
 
 	override fun onPeriodElementClick(dialog: DialogFragment, element: PeriodElement?) {
-
+		// Ignore single clicks, wait for onPositiveButtonClick instead
 	}
 
 	override fun onPositiveButtonClicked(dialog: ElementPickerDialog) {
@@ -196,7 +195,7 @@ class RoomFinderActivity : BaseActivity(), ElementPickerDialog.ElementPickerDial
 	}
 
 	override fun onExpiredClick(position: Int) {
-		//refreshItem(position)
+		// TODO: Refresh item
 	}
 
 	private fun addRooms(rooms: List<PeriodElement>) {
@@ -259,35 +258,6 @@ class RoomFinderActivity : BaseActivity(), ElementPickerDialog.ElementPickerDial
 
 		refreshRoomList()
 	}
-
-	/*private fun reload() {
-		roomList!!.clear()
-		try {
-			val reader = BufferedReader(InputStreamReader(openFileInput("roomList.txt")))
-			var name: String
-			while ((name = reader.readLine()) != null) {
-				val roomItem = AdapterItemRoomFinder(this, name, isInRequestQueue(name))
-				val binaryData = reader.readLine()
-				val states = BooleanArray(binaryData.length)
-				for (i in states.indices)
-					states[i] = binaryData[i] == '1'
-				roomItem.setStates(states)
-				maxHourIndex = states.size - 1
-				roomItem.setDate(java.lang.Long.parseLong(reader.readLine()))
-				roomList!!.add(roomItem)
-			}
-		} catch (e: IOException) {
-			e.printStackTrace()
-		}
-
-		for (r in requestQueue!!) {
-			if (!r.isRefreshOnly)
-				roomList!!.add(AdapterItemRoomFinder(this, r.displayName, true))
-			roomAdapter!!.notifyItemInserted(roomList!!.size - 1)
-		}
-
-		refreshRoomList()
-	}*/
 
 	private fun refreshRoomList() {
 		if (roomList.isNotEmpty())
@@ -357,248 +327,4 @@ class RoomFinderActivity : BaseActivity(), ElementPickerDialog.ElementPickerDial
 		}
 		return null
 	}
-
-	/*private fun loadRoom(room: RequestModel) {
-		val unitManager: TimegridUnitManager
-		try {
-			unitManager = TimegridUnitManager(MasterData(ListManager.getUserData(application).getJSONObject("masterData")))
-		} catch (e: JSONException) {
-			e.printStackTrace()
-			// TODO: Proper error handling
-			return
-		}
-
-		val days = unitManager.getNumberOfDays()
-		val hours = unitManager.getHoursPerDay()
-
-		val startDateFromWeek = Integer.parseInt(SimpleDateFormat("yyyyMMdd", Locale.ENGLISH)
-				.format(getStartDateFromWeek(Calendar.getInstance(), 0).getTime()))
-
-		val prefs = getSharedPreferences("loginData", Context.MODE_PRIVATE)
-		val sessionInfo = SessionInfo()
-		sessionInfo.setElemId(room.roomID)
-		sessionInfo.setElemType(getElemTypeName(ROOM))
-
-		val api = UntisRequest(this, sessionInfo)
-
-		val handler = { response ->
-			if (response == null) {
-				Log.w("ActivityRoomFinder", "response is null")
-				// TODO: Stop loading and show "unknown error: null";
-				return
-			}
-			try {
-				if (response!!.has("error")) {
-					Log.w("error", response!!.toString())
-					Snackbar.make(recyclerView,
-							getString(R.string.snackbar_error, response!!.getJSONObject("error")
-									.getString("message")), Snackbar.LENGTH_LONG)
-							.setAction("OK", null).show()
-				} else if (response!!.has("result")) {
-					val requestModel = requestQueue!![0]
-
-					val timetable = Timetable(response!!.getJSONObject("result"), PreferenceManager.getDefaultSharedPreferences(applicationContext))
-
-					val states = BooleanArray(days * hours)
-
-					for (i in states.indices) {
-						val day = i / hours
-						val hour = i % hours
-
-						if (timetable.getItems(day, hour).size() > 0)
-							states[day * hours + hour] = true
-					}
-
-					val binaryData = StringBuilder()
-					for (value in states)
-						binaryData.append(if (value) '1' else '0')
-
-					if (!TextUtils.isEmpty(binaryData.toString())) {
-						if (requestModel.isRefreshOnly)
-							deleteItem(requestModel.displayName)
-
-						val writer = BufferedWriter(OutputStreamWriter(
-								openFileOutput("roomList.txt", Context.MODE_APPEND), "UTF-8"))
-						writer.write(requestModel.displayName!!)
-						writer.newLine()
-						writer.write(binaryData.toString())
-						writer.newLine()
-						writer.write(String.valueOf(getStartDateFromWeek(Calendar.getInstance(), 0,
-								true).getTimeInMillis()))
-						writer.newLine()
-						writer.close()
-					}
-
-					reload()
-					refreshRoomList()
-				}
-			} catch (e: JSONException) {
-				e.printStackTrace()
-			} catch (e: IOException) {
-				e.printStackTrace()
-			}
-
-			requestQueue!!.removeAt(0)
-			executeRequestQueue()
-		}
-
-		val query = UntisRequest.UntisRequestQuery()
-		query.setMethod(Constants.UntisAPI.METHOD_GET_TIMETABLE)
-		query.setHost(prefs.getString("url", null))
-		query.setPath("/WebUntis/jsonrpc_intern.do")
-		query.setSchool(prefs.getString("school", null))
-
-		val params = JSONObject()
-		try {
-			params
-					.put("id", room.roomID)
-					.put("type", getElemTypeName(ROOM))
-					.put("startDate", startDateFromWeek)
-					.put("endDate", addDaysToInt(startDateFromWeek, days))
-					.put("masterDataTimestamp", System.currentTimeMillis())
-					.put("auth", getAuthObject(prefs.getString("user", ""), prefs.getString("key", "")))
-		} catch (e: JSONException) {
-			e.printStackTrace() // TODO: Implment proper error handling (search for possible cases first)
-		}
-
-		query.setParams(JSONArray().put(params))
-
-		api.setCachingMode(UntisRequest.CachingMode.RETURN_CACHE_LOAD_LIVE)
-		api.setResponseHandler(handler).submit(query)
-	}
-
-	fun getCurrentHourIndex(): Int {
-		if (currentHourIndex >= 0)
-			return currentHourIndex + hourIndex
-
-		var index = 0
-
-		var masterData: MasterData? = null
-		try {
-			masterData = MasterData(ListManager.getUserData(application).getJSONObject("masterData"))
-		} catch (e: JSONException) {
-			e.printStackTrace()
-		}
-
-		val unitManager = TimegridUnitManager(masterData)
-
-		val cNow = Calendar.getInstance()
-		val cToCompare = Calendar.getInstance()
-
-		val startDateFromWeek = Integer.parseInt(SimpleDateFormat("yyyyMMdd", Locale.ENGLISH)
-				.format(DateOperations.getStartDateFromWeek(Calendar.getInstance(), 0).getTime()))
-
-		for (i in 0 until unitManager.getNumberOfDays() * unitManager.getHoursPerDay()) {
-			val dateTime = (addDaysToInt(startDateFromWeek, i / unitManager.getHoursPerDay(),
-					SimpleDateFormat("yyyy-MM-dd'T'", Locale.ENGLISH))
-					+ String.format("%1$5s", unitManager.getUnits().get(i % unitManager
-					.getHoursPerDay()).getDisplayEndTime()).replace(' ', '0') + "Z")
-
-			try {
-				cToCompare.time = DateOperations.parseFromISO(dateTime)
-			} catch (e: ParseException) {
-				e.printStackTrace()
-			}
-
-			if (cNow.timeInMillis > cToCompare.timeInMillis)
-				index++
-			else
-				break
-		}
-
-		if (index == unitManager.getNumberOfDays() * unitManager.getHoursPerDay())
-			index = 0
-
-		Log.d("RoomFinder", "Current Hour Index: $index")
-		currentHourIndex = Math.max(index, 0)
-		return currentHourIndex + hourIndex
-	}
-
-	private fun deleteItem(name: String?): Boolean {
-		val inputFile = File(filesDir, "roomList.txt")
-		val tempFile = File(filesDir, "roomList.txt.tmp")
-
-		val reader = BufferedReader(InputStreamReader(
-				openFileInput("roomList.txt"), "UTF-8"))
-		val writer = BufferedWriter(OutputStreamWriter(
-				openFileOutput("roomList.txt.tmp", Context.MODE_APPEND), "UTF-8"))
-
-		var currentLine: String
-
-		while ((currentLine = reader.readLine()) != null) {
-			val trimmedLine = currentLine.trim { it <= ' ' }
-
-			if (trimmedLine == name) {
-				reader.readLine()
-				reader.readLine()
-				continue
-			}
-
-			writer.write(currentLine)
-			writer.newLine()
-			writer.write(reader.readLine())
-			writer.newLine()
-			writer.write(reader.readLine())
-			writer.newLine()
-		}
-		reader.close()
-		writer.close()
-
-		return inputFile.delete() && tempFile.renameTo(inputFile)
-	}
-
-	fun refreshItem(position: Int) {
-		AlertDialog.Builder(this)
-				.setTitle(R.string.refresh_item_title)
-				.setMessage(getString(R.string.refresh_item_text))
-				.setPositiveButton(R.string.refresh_this_item, { dialog, which ->
-					refreshItemData(position)
-					roomAdapter!!.notifyDataSetChanged()
-					executeRequestQueue()
-					dialog.dismiss()
-				})
-				.setNeutralButton(R.string.refresh_all_items, { dialog, which ->
-					for (i in roomList!!.indices)
-						if (roomList!![i].isOutdated())
-							refreshItemData(i)
-					roomAdapter!!.notifyDataSetChanged()
-					executeRequestQueue()
-					dialog.dismiss()
-				})
-				.setNegativeButton(R.string.cancel, { dialog, which -> dialog.dismiss() })
-				.create()
-				.show()
-	}
-
-	private fun refreshItemData(position: Int) {
-		roomList!![position].setLoading()
-
-		val elementName = ElementName(ROOM, userDataList)
-
-		try {
-			requestQueue!!.add(RequestModel(elementName.findFieldByValue("name",
-					roomList!![position].getName(), "id") as Int, roomList!![position].getName(), true))
-		} catch (e: JSONException) {
-			e.printStackTrace() // Not expected to occur
-		}
-
-	}
-
-	override fun onClick(v: View) {
-		val itemPosition = recyclerView!!.getChildLayoutPosition(v)
-		val item = roomList!![itemPosition]
-
-		val intent = Intent()
-		val elementName = ElementName(ROOM, userDataList)
-		try {
-			intent.putExtra("elemId", elementName.findFieldByValue("name", item.getName(), "id") as Int)
-		} catch (e: JSONException) {
-			e.printStackTrace() // Not expected to occur
-		}
-
-		intent.putExtra("elemType", ROOM)
-		intent.putExtra("displayName", getString(R.string.title_room, item.getName()))
-		setResult(Activity.RESULT_OK, intent)
-		finish()
-	}*/
 }
