d98eb8ef91c4f6145747d903cf3009f6d84d909b
SapuSeven <sapuseven@gmail.com>
2019-09-25 22:03:19 +0200

    Fixed display of holidays ranging over multiple years

--- androidweekview/src/main/java/com/alamkanak/weekview/HolidayChip.kt
+++ androidweekview/src/main/java/com/alamkanak/weekview/HolidayChip.kt
@@ -1,22 +1,19 @@
 package com.alamkanak.weekview
 
 import org.joda.time.DateTime
+import org.joda.time.Interval
 import org.joda.time.format.DateTimeFormatter
 import org.joda.time.format.ISODateTimeFormat
 
 data class HolidayChip(val text: String = "Test", val startDate: String, val endDate: String) {
-	private val startDateCalendar = parseDate(startDate)
-	private val endDateCalendar = parseDate(endDate)
+	private val startDateTime = parseDate(startDate)
+	private val endDateTime = parseDate(endDate)
 
 	companion object {
 		val DATE_FORMAT: DateTimeFormatter = ISODateTimeFormat.date()
 	}
 
-	fun isOnDay(day: DateTime) =
-			day.year >= startDateCalendar.year
-					&& day.dayOfYear >= startDateCalendar.dayOfYear
-					&& day.year <= endDateCalendar.year
-					&& day.dayOfYear <= endDateCalendar.dayOfYear
+	fun isOnDay(day: DateTime) = Interval(startDateTime, endDateTime.millisOfDay().withMaximumValue()).contains(day)
 
 	private fun parseDate(date: String) = DATE_FORMAT.parseDateTime(date)
 }
