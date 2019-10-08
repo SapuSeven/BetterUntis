c19c879fdd0f946c7905d19b49df583aa2bc610d
SapuSeven <sapuseven@gmail.com>
2019-10-08 10:09:32 +0200

    Added MessengerSettings data structure

--- app/src/main/java/com/sapuseven/untis/models/UntisUserData.kt
+++ app/src/main/java/com/sapuseven/untis/models/UntisUserData.kt
@@ -1,6 +1,7 @@
 package com.sapuseven.untis.models
 
 import com.sapuseven.untis.models.untis.MasterData
+import com.sapuseven.untis.models.untis.MessengerSettings
 import com.sapuseven.untis.models.untis.Settings
 import com.sapuseven.untis.models.untis.UserData
 import kotlinx.serialization.Serializable
@@ -10,9 +11,5 @@ data class UntisUserData(
 		val masterData: MasterData,
 		val userData: UserData,
 		val settings: Settings,
-		val messengerSettings: UnknownObject? = null // This is a new element with unknown usage
-) {
-	init {
-		UnknownObject.validate(mapOf("messengerSettings" to messengerSettings))
-	}
-}
+		val messengerSettings: MessengerSettings? = null
+)
