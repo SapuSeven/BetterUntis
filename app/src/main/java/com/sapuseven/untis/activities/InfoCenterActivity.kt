f59cf3b875c27d27cf267af0e7c1ab339fbd49c5
SapuSeven <sapuseven@gmail.com>
2019-10-14 18:25:39 +0200

    Added Info-Center activity

---
+++ app/src/main/java/com/sapuseven/untis/activities/InfoCenterActivity.kt
@@ -0,0 +1,11 @@
+package com.sapuseven.untis.activities
+
+import android.os.Bundle
+import com.sapuseven.untis.R
+
+class InfoCenterActivity : BaseActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_infocenter)
+	}
+}
