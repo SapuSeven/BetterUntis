6cf2b2931a8879fdf6cfed8a52ef4eceb0c85750
SapuSeven <sapuseven@gmail.com>
2019-10-31 11:27:30 +0100

    Only draw lesson start- and end-times if enough space

--- androidweekview/src/main/java/com/alamkanak/weekview/drawers/TimeColumnDrawer.kt
+++ androidweekview/src/main/java/com/alamkanak/weekview/drawers/TimeColumnDrawer.kt
@@ -12,17 +12,17 @@ class TimeColumnDrawer(private val config: WeekViewConfig) : BaseDrawer {
 
 	override fun draw(drawingContext: DrawingContext, canvas: Canvas) {
 		var top = drawConfig.headerHeight
-		val bottom = WeekView.viewHeight
+		val bottom = WeekView.viewHeight.toFloat()
 
 		val bottomTimeOffset = 8f
 
-		// Draw the background color for the header column.
-		canvas.drawRect(0f, top, drawConfig.timeColumnWidth, bottom.toFloat(), drawConfig.timeColumnBackgroundPaint)
+		// Draw the background color for the time column.
+		canvas.drawRect(0f, top, drawConfig.timeColumnWidth, bottom, drawConfig.timeColumnBackgroundPaint)
 
 		canvas.restore()
 		canvas.save()
 
-		canvas.clipRect(0f, top, drawConfig.timeColumnWidth, bottom.toFloat())
+		canvas.clipRect(0f, top, drawConfig.timeColumnWidth, bottom)
 
 		// The original header height
 		val headerHeight = top
@@ -34,27 +34,27 @@ class TimeColumnDrawer(private val config: WeekViewConfig) : BaseDrawer {
 
 			val lastHourTop = if (i > 0) config.hourHeight * (config.hourLines[i - 1] - config.startTime) / 60.0f else 0.0f
 
-			// Draw the text if its y position is not outside of the visible area. The pivot point
-			// of the text is the point at the bottom-right corner.
+			// Draw the text if its y position is not outside of the visible area.
+			// The pivot point of the text is the point at the bottom-right corner.
 			val time = drawConfig.dateTimeInterpreter.interpretTime(config.hourLines[i])
 
 			if (top - (hourTop - lastHourTop) < bottom) {
-				if (i % 2 == 0)
-					canvas.drawText(time, config.timeColumnPadding.toFloat(), top + drawConfig.timeTextHeight, drawConfig.timeTextTopPaint)
-				else
-					canvas.drawText(time, config.timeColumnPadding + drawConfig.timeTextWidth, top - bottomTimeOffset, drawConfig.timeTextBottomPaint)
+				if (i % 2 == 0) {
+					if (config.hourLines[i + 1] - config.hourLines[i] > 30)
+						canvas.drawText(time, config.timeColumnPadding.toFloat(), top + drawConfig.timeTextHeight, drawConfig.timeTextTopPaint)
+				} else
+					if (config.hourLines[i] - config.hourLines[i - 1] > 30)
+						canvas.drawText(time, config.timeColumnPadding + drawConfig.timeTextWidth, top - bottomTimeOffset, drawConfig.timeTextBottomPaint)
 
-
-				if (i % 2 == 1) {
+				if (i % 2 == 1)
 					canvas.drawText((i / 2 + 1).toString(), config.timeColumnPadding + drawConfig.timeTextWidth / 2, top - (hourTop - lastHourTop) / 2 + bottomTimeOffset * 1.5f, drawConfig.timeCaptionPaint)
-				}
 			}
 		}
 
 		// Draw the vertical time column separator
 		if (config.showTimeColumnSeparator) {
 			val lineX = drawConfig.timeColumnWidth - config.timeColumnSeparatorStrokeWidth
-			canvas.drawLine(lineX, drawConfig.headerHeight, lineX, bottom.toFloat(), drawConfig.timeColumnSeparatorPaint)
+			canvas.drawLine(lineX, drawConfig.headerHeight, lineX, bottom, drawConfig.timeColumnSeparatorPaint)
 		}
 
 		canvas.restore()
