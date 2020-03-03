e5e1f400947615a9c7b720c4aff45ae708e1f073
Domi04151309 <domi04151309business@gmail.com>
2020-03-03 11:02:53 +0100

    Add wear module

---
+++ wear/src/main/java/com/sapuseven/untis/wear/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sapuseven.untis.wear
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+
+class MainActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Enables Always-on
+        setAmbientEnabled()
+    }
+}
