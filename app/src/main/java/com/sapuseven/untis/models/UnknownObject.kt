16f91392b62765f36084bd2102871930bd049e1a
SapuSeven <sapuseven@gmail.com>
2020-03-05 08:55:25 +0100

    Fix UnknownObject equals() function

--- app/src/main/java/com/sapuseven/untis/models/UnknownObject.kt
+++ app/src/main/java/com/sapuseven/untis/models/UnknownObject.kt
@@ -27,4 +27,19 @@ class UnknownObject(val jsonString: String?) {
 			}
 		}
 	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (javaClass != other?.javaClass) return false
+
+		other as UnknownObject
+
+		if (jsonString != other.jsonString) return false
+
+		return true
+	}
+
+	override fun hashCode(): Int {
+		return jsonString?.hashCode() ?: 0
+	}
 }
