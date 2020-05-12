4bf4b4c31e0df210823c20f42c6d9909b434edd0
Domi04151309 <domi04151309business@gmail.com>
2020-05-12 18:45:48 +0200

    Display attachments

--- app/src/main/java/com/sapuseven/untis/adapters/infocenter/MessageAdapter.kt
+++ app/src/main/java/com/sapuseven/untis/adapters/infocenter/MessageAdapter.kt
@@ -1,11 +1,14 @@
 package com.sapuseven.untis.adapters.infocenter
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.text.method.LinkMovementMethod
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -17,7 +20,7 @@ class MessageAdapter(
 		private val messageList: List<UntisMessage> = ArrayList()
 ) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-		val v = LayoutInflater.from(parent.context).inflate(R.layout.list_item_twoline, parent, false)
+		val v = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
 		return ViewHolder(v)
 	}
 
@@ -26,17 +29,32 @@ class MessageAdapter(
 	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 		val message = messageList[position]
 
+		var attachmentTitles: Array<CharSequence> = arrayOf()
+		var attachmentURLs: Array<String> = arrayOf()
+		message.attachments.forEach {
+			attachmentTitles += it.name
+			attachmentURLs += it.url
+		}
+
 		holder.tvSubject.text = message.subject
 		holder.tvBody.text = HtmlCompat.fromHtml(message.body, HtmlCompat.FROM_HTML_MODE_COMPACT)
 		holder.tvBody.movementMethod = LinkMovementMethod.getInstance()
-
-		Log.e(this.javaClass.simpleName, message.attachments.toString())
+		holder.btnAttachments.setOnClickListener {
+			AlertDialog.Builder(context)
+					.setTitle(R.string.infocenter_messages_attachments)
+					.setItems(attachmentTitles) { _, which ->
+						context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(attachmentURLs[which])))
+					}
+					.show()
+		}
 
 		holder.tvSubject.visibility = if (message.subject.isBlank()) View.GONE else View.VISIBLE
+		holder.btnAttachments.visibility = if (message.attachments.isEmpty()) View.GONE else View.VISIBLE
 	}
 
 	class ViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
-		val tvSubject: TextView = rootView.findViewById(R.id.textview_listitem_line1)
-		val tvBody: TextView = rootView.findViewById(R.id.textview_listitem_line2)
+		val tvSubject: TextView = rootView.findViewById(R.id.textview_itemmessage_subject)
+		val tvBody: TextView = rootView.findViewById(R.id.textview_itemmessage_body)
+		val btnAttachments: Button = rootView.findViewById(R.id.button_itemmessage_attachment)
 	}
 }
