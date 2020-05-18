4581e6cccb89bdae873fbf920a83b2e95d61e243
SapuSeven <sapuseven@gmail.com>
2020-05-18 21:47:24 +0200

    Switch to material design dialogs for unified styles

--- app/src/main/java/com/sapuseven/untis/adapters/infocenter/MessageAdapter.kt
+++ app/src/main/java/com/sapuseven/untis/adapters/infocenter/MessageAdapter.kt
@@ -1,6 +1,5 @@
 package com.sapuseven.untis.adapters.infocenter
 
-import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -12,7 +11,9 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.sapuseven.untis.R
+import com.sapuseven.untis.models.UntisAttachment
 import com.sapuseven.untis.models.UntisMessage
 
 class MessageAdapter(
@@ -29,6 +30,12 @@ class MessageAdapter(
 	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 		val message = messageList[position]
 
+		message.attachments = listOf(
+				UntisAttachment(1, "Name 1", ""),
+				UntisAttachment(2, "Name 2", ""),
+				UntisAttachment(3, "Name 3", "")
+		)
+
 		var attachmentTitles: Array<CharSequence> = arrayOf()
 		var attachmentURLs: Array<String> = arrayOf()
 		message.attachments.forEach {
@@ -40,7 +47,7 @@ class MessageAdapter(
 		holder.tvBody.text = HtmlCompat.fromHtml(message.body, HtmlCompat.FROM_HTML_MODE_COMPACT)
 		holder.tvBody.movementMethod = LinkMovementMethod.getInstance()
 		holder.btnAttachments.setOnClickListener {
-			AlertDialog.Builder(context)
+			MaterialAlertDialogBuilder(context)
 					.setTitle(R.string.infocenter_messages_attachments)
 					.setItems(attachmentTitles) { _, which ->
 						context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(attachmentURLs[which])))
