3b22a11e1228d89133a5c43a3bd880e905996ae4
Domi04151309 <domi04151309business@gmail.com>
2020-07-17 20:25:29 +0200

    Print timetable to log

--- app/src/main/java/com/sapuseven/untis/widgets/BaseWidget.kt
+++ app/src/main/java/com/sapuseven/untis/widgets/BaseWidget.kt
@@ -6,15 +6,20 @@ import android.content.Context
 import android.widget.RemoteViews
 import com.sapuseven.untis.R
 import com.sapuseven.untis.data.databases.UserDatabase
+import java.lang.Exception
 
 open class BaseWidget : AppWidgetProvider() {
 
-    protected var user: UserDatabase.User? = null
+    protected var userId: Long = 0
+    protected lateinit var user: UserDatabase.User
+    protected lateinit var userDatabase: UserDatabase
     protected lateinit var views: RemoteViews
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            try {
+                updateAppWidget(context, appWidgetManager, appWidgetId)
+            } catch (e: Exception) { }
         }
     }
 
@@ -25,11 +30,12 @@ open class BaseWidget : AppWidgetProvider() {
     }
 
     open fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-        val userId = loadIdPref(context, appWidgetId)
-        user = UserDatabase.createInstance(context).getUser(userId)
+        userDatabase = UserDatabase.createInstance(context)
+        userId = loadIdPref(context, appWidgetId)
+        user = userDatabase.getUser(userId) ?: onUnknownUser(context, appWidgetManager, appWidgetId)
         views = RemoteViews(context.packageName, R.layout.base_widget)
-        views.setTextViewText(R.id.textview_daily_messages_widget_account, user?.userData?.displayName)
-        views.setTextViewText(R.id.textview_daily_messages_widget_school, user?.userData?.schoolName)
+        views.setTextViewText(R.id.textview_daily_messages_widget_account, user.userData.displayName)
+        views.setTextViewText(R.id.textview_daily_messages_widget_school, user.userData.schoolName)
 
         val colorPrimary = when (context.getSharedPreferences("preferences_$userId", Context.MODE_PRIVATE).getString("preference_theme", null)) {
             "untis" -> R.color.colorPrimaryThemeUntis
@@ -41,6 +47,14 @@ open class BaseWidget : AppWidgetProvider() {
             else -> R.color.colorPrimary
         }
         views.setInt(R.id.linearlayout_daily_messages_widget_top_bar, "setBackgroundColor", context.resources.getColor(colorPrimary))
+    }
+
+    private fun onUnknownUser(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int): Nothing {
+        views = RemoteViews(context.packageName, R.layout.base_widget)
+        views.setTextViewText(R.id.textview_daily_messages_widget_account, context.resources.getString(R.string.all_error))
+        views.setTextViewText(R.id.textview_daily_messages_widget_school, context.resources.getString(R.string.all_error))
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+        throw Exception()
     }
 }
 
--- app/src/main/java/com/sapuseven/untis/widgets/TimetableWidget.kt
+++ app/src/main/java/com/sapuseven/untis/widgets/TimetableWidget.kt
@@ -2,12 +2,43 @@ package com.sapuseven.untis.widgets
 
 import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.util.Log
 import com.sapuseven.untis.R
+import com.sapuseven.untis.data.timetable.TimegridItem
+import com.sapuseven.untis.helpers.timetable.TimetableDatabaseInterface
+import com.sapuseven.untis.helpers.timetable.TimetableLoader
+import com.sapuseven.untis.interfaces.TimetableDisplay
+import com.sapuseven.untis.models.untis.UntisDate
+import org.joda.time.LocalDate
+import java.lang.ref.WeakReference
 
-class TimetableWidget : BaseWidget() {
+class TimetableWidget : BaseWidget(), TimetableDisplay {
+
+    private lateinit var timetableDatabaseInterface: TimetableDatabaseInterface
+    private lateinit var timetableLoader: TimetableLoader
+
     override fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
         super.updateAppWidget(context, appWidgetManager, appWidgetId)
         views.setTextViewText(R.id.textview_daily_messages_widget_content, "TODO: Display timetable for current day")
+
+        timetableDatabaseInterface = TimetableDatabaseInterface(userDatabase, userId)
+        timetableLoader = TimetableLoader(WeakReference(context), this, user, timetableDatabaseInterface)
+        loadTimetable()
+
         appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    override fun addTimetableItems(items: List<TimegridItem>, startDate: UntisDate, endDate: UntisDate, timestamp: Long) {
+        Log.d("BetterUntis", items.toString())
+    }
+
+    override fun onTimetableLoadingError(requestId: Int, code: Int?, message: String?) {
+        Log.e("BetterUntis", message)
+    }
+
+    private fun loadTimetable(force: Boolean = false) {
+        val today = UntisDate.fromLocalDate(LocalDate.now())
+        val flags = if (force) TimetableLoader.FLAG_LOAD_SERVER else TimetableLoader.FLAG_LOAD_CACHE
+        timetableLoader.load(TimetableLoader.TimetableLoaderTarget(today, today, user.userData.elemId, user.userData.elemType ?: ""), flags)
     }
 }
