06f25e386a22b0ca5bf699d174cdb4a2d250d45a
Domi04151309 <domi04151309business@gmail.com>
2020-07-25 20:24:18 +0200

    Remove suppression

--- app/src/main/java/com/sapuseven/untis/adapters/infocenter/AbsenceAdapter.kt
+++ app/src/main/java/com/sapuseven/untis/adapters/infocenter/AbsenceAdapter.kt
@@ -1,6 +1,5 @@
 package com.sapuseven.untis.adapters.infocenter
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -28,7 +27,6 @@ class AbsenceAdapter(
 
 	override fun getItemCount() = absenceList.size
 
-	@SuppressLint("SetTextI18n")
 	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 		val absence = absenceList[position]
 
