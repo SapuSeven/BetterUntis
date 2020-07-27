2bd3243089f5b6f6eb783ca56275a32270d17bbd
Domi04151309 <domi04151309business@gmail.com>
2020-07-27 20:18:09 +0200

    Add basic tutorial

--- app/src/main/java/com/sapuseven/untis/activities/MainActivity.kt
+++ app/src/main/java/com/sapuseven/untis/activities/MainActivity.kt
@@ -34,10 +34,7 @@ import com.sapuseven.untis.R
 import com.sapuseven.untis.adapters.ProfileListAdapter
 import com.sapuseven.untis.data.databases.UserDatabase
 import com.sapuseven.untis.data.timetable.TimegridItem
-import com.sapuseven.untis.dialogs.DatePickerDialog
-import com.sapuseven.untis.dialogs.ElementPickerDialog
-import com.sapuseven.untis.dialogs.ErrorReportingDialog
-import com.sapuseven.untis.dialogs.TimetableItemDetailsDialog
+import com.sapuseven.untis.dialogs.*
 import com.sapuseven.untis.helpers.ConversionUtils
 import com.sapuseven.untis.helpers.DateTimeUtils
 import com.sapuseven.untis.helpers.ErrorMessageDictionary
@@ -153,6 +150,7 @@ class MainActivity :
 			setupTimetableLoader()
 			showPersonalTimetable()
 			refreshNavigationViewSelection()
+			checkTutorial()
 		}
 	}
 
@@ -746,6 +744,11 @@ class MainActivity :
 			TimetableDatabaseInterface.Type.ROOM.name -> (navigationview_main as NavigationView).setCheckedItem(R.id.nav_show_rooms)
 			else -> (navigationview_main as NavigationView).setCheckedItem(R.id.nav_show_personal)
 		}
+	}
+
+	private fun checkTutorial() {
+		if (!preferences.defaultPrefs.getBoolean("preference_has_finished_tutorial", false))
+			TutorialDialog(this, preferences.defaultPrefs, findViewById(R.id.drawer_layout)).show()
 	}
 
 	private fun showLessonInfo(item: TimegridItem) {
---
+++ app/src/main/java/com/sapuseven/untis/dialogs/TutorialDialog.kt
@@ -0,0 +1,99 @@
+package com.sapuseven.untis.dialogs
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.view.Gravity
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.sapuseven.untis.R
+
+class TutorialDialog(
+        private val context: Context,
+        private val prefs: SharedPreferences,
+        private val drawer: DrawerLayout
+) {
+
+    fun show(type: Byte = DIALOG_WELCOME, gravity: Int = Gravity.CENTER, dim: Float = 0.32f) {
+
+        val builder = AlertDialog.Builder(context)
+        when (type) {
+            DIALOG_WELCOME -> {
+                builder.setTitle(context.getString(R.string.tutorial_welcome_title))
+                builder.setMessage(context.getString(R.string.tutorial_welcome_message))
+                builder.setPositiveButton(context.getString(R.string.tutorial_next)) { _, _ ->
+                            show(DIALOG_TIMETABLE, Gravity.BOTTOM, 0f)
+                        }
+            }
+            DIALOG_TIMETABLE -> {
+                builder.setTitle(context.getString(R.string.tutorial_timetable_title))
+                builder.setMessage(context.getString(R.string.tutorial_timetable_message))
+                builder.setPositiveButton(context.getString(R.string.tutorial_next)) { _, _ ->
+                    show(DIALOG_TIMETABLE_ITEM, Gravity.BOTTOM, 0f)
+                }
+            }
+            DIALOG_TIMETABLE_ITEM -> {
+                builder.setTitle(context.getString(R.string.tutorial_timetable_item_title))
+                builder.setMessage(context.getString(R.string.tutorial_timetable_item_message))
+                builder.setPositiveButton(context.getString(R.string.tutorial_next)) { _, _ ->
+                    show(DIALOG_TIMETABLE_PICKER, Gravity.BOTTOM, 0f)
+                    drawer.openDrawer(GravityCompat.START)
+                }
+            }
+            DIALOG_TIMETABLE_PICKER -> {
+                builder.setTitle(context.getString(R.string.tutorial_timetable_picker_title))
+                builder.setMessage(context.getString(R.string.tutorial_timetable_picker_message))
+                builder.setPositiveButton(context.getString(R.string.tutorial_next)) { _, _ ->
+                            show(DIALOG_INFO_CENTER, Gravity.TOP, 0f)
+                        }
+            }
+            DIALOG_INFO_CENTER -> {
+                builder.setTitle(context.getString(R.string.tutorial_info_center_title))
+                builder.setMessage(context.getString(R.string.tutorial_info_center_message))
+                builder.setPositiveButton(context.getString(R.string.tutorial_next)) { _, _ ->
+                            show(DIALOG_FREE_ROOMS, Gravity.TOP, 0f)
+                        }
+            }
+            DIALOG_FREE_ROOMS -> {
+                builder.setTitle(context.getString(R.string.tutorial_free_rooms_title))
+                builder.setMessage(context.getString(R.string.tutorial_free_rooms_message))
+                builder.setPositiveButton(context.getString(R.string.tutorial_next)) { _, _ ->
+                            show(DIALOG_FINISH)
+                            drawer.closeDrawer(GravityCompat.START)
+                        }
+            }
+            DIALOG_FINISH -> {
+                builder.setTitle(context.getString(R.string.tutorial_finish_title))
+                builder.setMessage(context.getString(R.string.tutorial_finish_message))
+                builder.setPositiveButton(context.getString(R.string.tutorial_next)) { _, _ ->
+                            finishTutorial()
+                        }
+            }
+        }
+
+        builder.setCancelable(false)
+        builder.setNeutralButton(context.getString(R.string.tutorial_skip)){ _, _ ->
+                    finishTutorial()
+                }
+
+        val dialog = builder.create()
+        val window = dialog.window
+        window?.setDimAmount(dim)
+        window?.attributes?.gravity = gravity
+        dialog.show()
+    }
+
+    private fun finishTutorial() {
+        prefs.edit().putBoolean("preference_has_finished_tutorial", true).apply()
+    }
+
+    companion object {
+        private const val DIALOG_WELCOME: Byte = 0
+        private const val DIALOG_TIMETABLE: Byte = 1
+        private const val DIALOG_TIMETABLE_ITEM: Byte = 2
+        private const val DIALOG_TIMETABLE_PICKER: Byte = 3
+        private const val DIALOG_INFO_CENTER: Byte = 4
+        private const val DIALOG_FREE_ROOMS: Byte = 5
+        private const val DIALOG_FINISH: Byte = 6
+    }
+}
