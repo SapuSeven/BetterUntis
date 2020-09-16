e81b2a1f328be532e0444c353c4f7502aadf0c20
Domi04151309 <domi04151309business@gmail.com>
2020-09-16 21:16:10 +0200

    Add additional information to generated reports

--- app/src/main/java/com/sapuseven/untis/helpers/issues/GithubIssue.kt
+++ app/src/main/java/com/sapuseven/untis/helpers/issues/GithubIssue.kt
@@ -3,33 +3,63 @@ package com.sapuseven.untis.helpers.issues
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
+import com.sapuseven.untis.R
 
 
 class GithubIssue(type: Type, log: String) : Issue(type, log) {
+
+	private var c: Context? = null
+
 	override fun launch(context: Context) {
+		c = context
 		val uri = Uri.Builder()
 				.scheme("https")
 				.authority("github.com")
 				.path("/SapuSeven/BetterUntis/issues/new")
 				.appendQueryParameter("title", generateTitle())
 				.appendQueryParameter("body", generateBody())
+				.appendQueryParameter("labels", "bug") //TODO: Make this label thing work
 
 		val browserIntent = Intent(Intent.ACTION_VIEW, uri.build())
 		context.startActivity(browserIntent)
 	}
 
 	private fun generateTitle() = when (type) {
-		Type.CRASH -> "Crash Report"
-		Type.EXCEPTION -> "Bug Report"
+		Type.CRASH -> "[Crash Report]"
+		Type.EXCEPTION -> "[Bug Report]"
 		Type.OTHER -> ""
 	}
 
+	private val version: String get() {
+		val pInfo = c?.packageManager?.getPackageInfo(c?.packageName ?: return "", 0)
+		return c?.getString(R.string.preference_info_app_version_desc,
+				pInfo?.versionName,
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+					pInfo?.longVersionCode
+				} else {
+					@Suppress("DEPRECATION")
+					pInfo?.versionCode?.toLong()
+				}
+		) ?: ""
+	}
+
+	private val installationSource: String get() {
+		return c?.packageManager?.getInstallerPackageName(c?.packageName ?:"") ?: ""
+	}
+
 	private fun generateBody() =
-			"<details>\n" +
-					"<summary>Crash Log</summary>\n" +
-					"\n" +
-					"```\n" +
-					"$log\n" +
-					"```\n" +
-					"</details>"
+		"<details>\n" +
+		"<summary>Logs</summary>\n" +
+		"\n" +
+		"```\n" +
+		"$log\n" +
+		"```\n" +
+		"</details>\n" +
+		"\n" +
+		"**Additional information**\n" +
+		"\n" +
+		"- Android version: _${Build.VERSION.RELEASE}_\n" +
+		"- BetterUntis version: _${version}_\n" +
+		"- Installation source: _${installationSource}_"
 }
