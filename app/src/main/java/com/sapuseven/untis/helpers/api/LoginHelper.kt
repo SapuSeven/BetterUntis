7835a5cf0fc6e7b3b361cdbbe89f5c76d67e0f0b
SapuSeven <sapuseven@gmail.com>
2021-12-09 22:07:32 +0100

    Add login SerializationException catching

--- app/src/main/java/com/sapuseven/untis/helpers/api/LoginHelper.kt
+++ app/src/main/java/com/sapuseven/untis/helpers/api/LoginHelper.kt
@@ -15,10 +15,15 @@ import com.sapuseven.untis.models.untis.response.SchoolSearchResponse
 import com.sapuseven.untis.models.untis.response.UserDataResponse
 import com.sapuseven.untis.models.untis.response.UserDataResult
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import java.net.UnknownHostException
 
-class LoginHelper(val loginData: LoginDataInfo, val onStatusUpdate: (statusStringRes: Int) -> Unit, val onError: (error: LoginErrorInfo) -> Unit) {
+class LoginHelper(
+	val loginData: LoginDataInfo,
+	val onStatusUpdate: (statusStringRes: Int) -> Unit,
+	val onError: (error: LoginErrorInfo) -> Unit
+) {
 	private val api: UntisRequest = UntisRequest()
 	private val proxyHost: String? = null // TODO: Pass proxy host
 
@@ -37,29 +42,52 @@ class LoginHelper(val loginData: LoginDataInfo, val onStatusUpdate: (statusStrin
 		query.url = UntisApiConstants.SCHOOL_SEARCH_URL
 		query.proxyHost = proxyHost
 		query.data.params =
-				if (schoolId != null) listOf(SchoolSearchParams(schoolid = schoolId))
-				else listOf(SchoolSearchParams(search = school))
+			if (schoolId != null) listOf(SchoolSearchParams(schoolid = schoolId))
+			else listOf(SchoolSearchParams(search = school))
 
 		val result = api.request(query)
 		result.fold({ data ->
-			val untisResponse = SerializationUtils.getJSON().decodeFromString<SchoolSearchResponse>(data)
+			try {
+				val untisResponse =
+					SerializationUtils.getJSON().decodeFromString<SchoolSearchResponse>(data)
 
-			untisResponse.result?.let {
-				if (it.schools.isNotEmpty()) {
-					val schoolResult = it.schools.find { schoolInfoResult ->
-						schoolInfoResult.schoolId == schoolId || schoolInfoResult.loginName.equals(school, true)
+				untisResponse.result?.let {
+					if (it.schools.isNotEmpty()) {
+						val schoolResult = it.schools.find { schoolInfoResult ->
+							schoolInfoResult.schoolId == schoolId || schoolInfoResult.loginName.equals(
+								school,
+								true
+							)
+						}
+
+						// TODO: Show manual selection dialog when there are more than one results are returned
+						if (schoolResult != null)
+							return schoolResult
 					}
-
-					// TODO: Show manual selection dialog when there are more than one results are returned
-					if (schoolResult != null)
-						return schoolResult
+					onError(LoginErrorInfo(errorMessageStringRes = R.string.logindatainput_error_invalid_school))
+				} ?: run {
+					onError(
+						LoginErrorInfo(
+							errorCode = untisResponse.error?.code,
+							errorMessage = untisResponse.error?.message
+						)
+					)
 				}
-				onError(LoginErrorInfo(errorMessageStringRes = R.string.logindatainput_error_invalid_school))
-			} ?: run {
-				onError(LoginErrorInfo(errorCode = untisResponse.error?.code, errorMessage = untisResponse.error?.message))
+			} catch (e: SerializationException) {
+				onError(
+					LoginErrorInfo(
+						errorMessageStringRes = R.string.logindatainput_error_generic,
+						errorMessage = e.message
+					)
+				)
 			}
 		}, { error ->
-			onError(LoginErrorInfo(errorMessageStringRes = R.string.logindatainput_error_generic, errorMessage = error.message))
+			onError(
+				LoginErrorInfo(
+					errorMessageStringRes = R.string.logindatainput_error_generic,
+					errorMessage = error.message
+				)
+			)
 		})
 
 		return null
@@ -78,18 +106,38 @@ class LoginHelper(val loginData: LoginDataInfo, val onStatusUpdate: (statusStrin
 		val appSharedSecretResult = api.request(query)
 
 		appSharedSecretResult.fold({ data ->
-			val untisResponse = SerializationUtils.getJSON().decodeFromString<AppSharedSecretResponse>(data)
+			try {
+				val untisResponse =
+					SerializationUtils.getJSON().decodeFromString<AppSharedSecretResponse>(data)
 
-			if (untisResponse.error?.code == ErrorMessageDictionary.ERROR_CODE_INVALID_CREDENTIALS)
-				return loginData.password
-			if (untisResponse.result.isNullOrEmpty())
-				onError(LoginErrorInfo(errorCode = untisResponse.error?.code, errorMessage = untisResponse.error?.message))
-			else
-				return untisResponse.result
+				if (untisResponse.error?.code == ErrorMessageDictionary.ERROR_CODE_INVALID_CREDENTIALS)
+					return loginData.password
+				if (untisResponse.result.isNullOrEmpty())
+					onError(
+						LoginErrorInfo(
+							errorCode = untisResponse.error?.code,
+							errorMessage = untisResponse.error?.message
+						)
+					)
+				else
+					return untisResponse.result
+			} catch (e: SerializationException) {
+				onError(
+					LoginErrorInfo(
+						errorMessageStringRes = R.string.logindatainput_error_generic,
+						errorMessage = e.message
+					)
+				)
+			}
 		}, { error ->
 			when (error.exception) {
 				is UnknownHostException -> onError(LoginErrorInfo(errorCode = ErrorMessageDictionary.ERROR_CODE_NO_SERVER_FOUND))
-				else -> onError(LoginErrorInfo(errorMessageStringRes = R.string.logindatainput_error_generic, errorMessage = error.message))
+				else -> onError(
+					LoginErrorInfo(
+						errorMessageStringRes = R.string.logindatainput_error_generic,
+						errorMessage = error.message
+					)
+				)
 			}
 		})
 
@@ -106,24 +154,52 @@ class LoginHelper(val loginData: LoginDataInfo, val onStatusUpdate: (statusStrin
 		query.data.method = UntisApiConstants.METHOD_GET_USER_DATA
 
 		if (loginData.anonymous)
-			query.data.params = listOf(UserDataParams(auth = UntisAuthentication.createAuthObject()))
+			query.data.params =
+				listOf(UserDataParams(auth = UntisAuthentication.createAuthObject()))
 		else {
 			if (key == null) return null
-			query.data.params = listOf(UserDataParams(auth = UntisAuthentication.createAuthObject(loginData.user, key)))
+			query.data.params = listOf(
+				UserDataParams(
+					auth = UntisAuthentication.createAuthObject(
+						loginData.user,
+						key
+					)
+				)
+			)
 		}
 
 		val userDataResult = api.request(query)
 
 		userDataResult.fold({ data ->
-			val untisResponse = SerializationUtils.getJSON().decodeFromString<UserDataResponse>(data) // TODO: Catch json parsing errors if response isn't valid json
+			try {
+				val untisResponse =
+					SerializationUtils.getJSON().decodeFromString<UserDataResponse>(data)
 
-			if (untisResponse.result != null) {
-				return untisResponse.result
-			} else {
-				onError(LoginErrorInfo(errorCode = untisResponse.error?.code, errorMessage = untisResponse.error?.message))
+				if (untisResponse.result != null) {
+					return untisResponse.result
+				} else {
+					onError(
+						LoginErrorInfo(
+							errorCode = untisResponse.error?.code,
+							errorMessage = untisResponse.error?.message
+						)
+					)
+				}
+			} catch (e: SerializationException) {
+				onError(
+					LoginErrorInfo(
+						errorMessageStringRes = R.string.logindatainput_error_generic,
+						errorMessage = e.message
+					)
+				)
 			}
 		}, { error ->
-			onError(LoginErrorInfo(errorMessageStringRes = R.string.logindatainput_error_generic, errorMessage = error.message))
+			onError(
+				LoginErrorInfo(
+					errorMessageStringRes = R.string.logindatainput_error_generic,
+					errorMessage = error.message
+				)
+			)
 		})
 
 		return null
