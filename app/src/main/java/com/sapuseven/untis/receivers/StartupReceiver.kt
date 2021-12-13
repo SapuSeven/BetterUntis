3bb36e772307648648754a22212e81a7a9597a4b
SapuSeven <sapuseven@gmail.com>
2021-12-13 22:22:14 +0100

    Create broadcasts for each profile

--- app/src/main/java/com/sapuseven/untis/receivers/StartupReceiver.kt
+++ app/src/main/java/com/sapuseven/untis/receivers/StartupReceiver.kt
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.sapuseven.untis.data.databases.UserDatabase
 import com.sapuseven.untis.helpers.config.PreferenceHelper
 import com.sapuseven.untis.receivers.LessonEventSetup.Companion.EXTRA_LONG_PROFILE_ID
 import org.joda.time.DateTime
@@ -17,27 +18,33 @@ class StartupReceiver : BroadcastReceiver() {
 	override fun onReceive(context: Context, intent: Intent) {
 		Log.d("StartupReceiver", "StartupReceiver received")
 		val preferences = PreferenceHelper(context)
-		// TODO: Repeat setup for every profile
-		/*if (!preferences.get<Boolean>("preference_notifications_enable")
-				&& !preferences.get<Boolean>("preference_automute_enable"))
-			return
+		val users = UserDatabase.createInstance(context)
 
-		val dateTime = DateTime().withTime(2, 0, 0, 0)
+		users.getAllUsers().forEach { user ->
+			preferences.loadProfile(user.id!!)
 
-		listOf(
-				Intent(context, NotificationSetup::class.java).apply {
-					putExtra(EXTRA_LONG_PROFILE_ID, preferences.loadProfileId())
+			val dateTime = DateTime().withTime(2, 0, 0, 0)
+
+			val broadcasts = mutableListOf<Intent>()
+
+			if (preferences.get<Boolean>("preference_notifications_enable"))
+				broadcasts.add(Intent(context, NotificationSetup::class.java).apply {
+					putExtra(EXTRA_LONG_PROFILE_ID, user.id)
 					putExtra(NotificationSetup.EXTRA_BOOLEAN_MANUAL, intent.getBooleanExtra(NotificationSetup.EXTRA_BOOLEAN_MANUAL, false))
-				},
-				Intent(context, AutoMuteSetup::class.java).apply {
-					putExtra(EXTRA_LONG_PROFILE_ID, preferences.loadProfileId())
-				}
-		).forEach {
-			val pendingIntent = PendingIntent.getBroadcast(context, 0, it, PendingIntent.FLAG_IMMUTABLE)
-			val am = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-			am.setInexactRepeating(AlarmManager.RTC_WAKEUP, dateTime.millis, AlarmManager.INTERVAL_DAY, pendingIntent)
+				})
 
-			context.sendBroadcast(it)
-		}*/
+			if (preferences.get<Boolean>("preference_automute_enable"))
+				broadcasts.add(Intent(context, AutoMuteSetup::class.java).apply {
+					putExtra(EXTRA_LONG_PROFILE_ID, user.id)
+				})
+
+			broadcasts.forEach {
+				val pendingIntent = PendingIntent.getBroadcast(context, 0, it, PendingIntent.FLAG_IMMUTABLE)
+				val am = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+				am.setInexactRepeating(AlarmManager.RTC_WAKEUP, dateTime.millis, AlarmManager.INTERVAL_DAY, pendingIntent)
+
+				context.sendBroadcast(it)
+			}
+		}
 	}
 }
