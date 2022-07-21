624cff7558ea890a720d62465b6505e9507d3112
SapuSeven <sapuseven@gmail.com>
2022-07-21 22:18:26 +0200

    Move Code Scan Activity to Material 3

--- app/src/main/java/com/sapuseven/untis/ui/theme/Theme.kt
+++ app/src/main/java/com/sapuseven/untis/ui/theme/Theme.kt
@@ -1,15 +1,11 @@
 package com.sapuseven.untis.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
@@ -77,7 +73,6 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun AppTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
-	// Dynamic color is available on Android 12+
 	dynamicColor: Boolean = true,
 	content: @Composable () -> Unit
 ) {
@@ -89,19 +84,10 @@ fun AppTheme(
 		darkTheme -> DarkColorScheme
 		else -> LightColorScheme
 	}
-	val view = LocalView.current
-	if (!view.isInEditMode) {
-		SideEffect {
-			(view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-			ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-		}
-	}
 
 	val systemUiController = rememberSystemUiController()
 
 	SideEffect {
-		// Update all of the system bar colors to be transparent, and use
-		// dark icons if we're in light theme
 		systemUiController.setSystemBarsColor(
 			color = colorScheme.background,
 			darkIcons = !darkTheme
