51d09b43efbcb8ee7e44e84df71fbbaf7af77efb
SapuSeven <sapuseven@gmail.com>
2022-11-27 03:49:08 +0100

    Fix unknown object serialization (fixes #150)

--- app/src/main/java/com/sapuseven/untis/models/UnknownObject.kt
+++ app/src/main/java/com/sapuseven/untis/models/UnknownObject.kt
@@ -11,11 +11,15 @@ import kotlinx.serialization.json.JsonDecoder
 
 @Serializable
 class UnknownObject(val jsonString: String?) {
+	@OptIn(ExperimentalSerializationApi::class)
 	@Serializer(forClass = UnknownObject::class)
 	companion object : KSerializer<UnknownObject> {
 		override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("UnknownObject", PrimitiveKind.STRING)
 
-		override fun serialize(encoder: Encoder, obj: UnknownObject) {}
+		@OptIn(ExperimentalSerializationApi::class)
+		override fun serialize(encoder: Encoder, value: UnknownObject) {
+			encoder.encodeNull()
+		}
 
 		override fun deserialize(decoder: Decoder): UnknownObject {
 			return UnknownObject((decoder as? JsonDecoder)?.decodeJsonElement().toString())
