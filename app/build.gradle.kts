c933f1717306f5af3b497d9875eb92863c91a5b9
SapuSeven <sapuseven@gmail.com>
2025-06-23 00:09:26 +0200

    Update to version 5.0.0-beta03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 		minSdk = 21
 		targetSdk = 35
 		versionCode = generateVersionCode()
-		versionName = "5.0.0-beta02"
+		versionName = "5.0.0-beta03"
 		vectorDrawables.useSupportLibrary = true
 		testInstrumentationRunner = "com.sapuseven.untis.HiltTestRunner"
 
